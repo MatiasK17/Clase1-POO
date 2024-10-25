47a36dad78629770e68446ed4b342531680b2fac
matia <matia@10.41.8.113>
2024-10-25 19:37:27 -0300

    Probando cositas

--- src/pooclase1/POOClase1.java
+++ src/pooclase1/POOClase1.java
@@ -22,6 +22,9 @@ public class POOClase1 {
         System.out.println(" Soy un objeto llamado: " + persona2.getNombre());
         
         System.out.println("Hola mi gente");
+        
+        System.out.println("probando cositas");
+        
   
     }
     
